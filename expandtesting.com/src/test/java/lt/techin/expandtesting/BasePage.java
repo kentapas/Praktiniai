dc761916519a1729882d9cdee2def1534acd8d87
kentapas <149999718+kentapas@users.noreply.github.com>
2024-02-05 11:44:10 +0200

    added log info

--- expandtesting.com/src/test/java/lt/techin/expandtesting/BasePage.java
+++ expandtesting.com/src/test/java/lt/techin/expandtesting/BasePage.java
@@ -4,22 +4,32 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.slf4j.Logger;
 
+import java.net.URL;
 import java.time.Duration;
 
+import static java.lang.invoke.MethodHandles.lookup;
+import static org.slf4j.LoggerFactory.getLogger;
+
 public class BasePage {
+    private static final Logger log = getLogger(lookup().lookupClass());
+
     WebDriver driver;
 
     @BeforeEach
     void setup(){
         driver = new FirefoxDriver();
-        driver.get("https://practice.expandtesting.com/notes/app");
+        String URL = "https://practice.expandtesting.com/notes/app";
+        driver.get(URL);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        log.info("Navigated to {}", URL);
     }
 
-//    @AfterEach
-//    void tearDown(){
-//        driver.close();
-//    }
+    @AfterEach
+    void tearDown(){
+        driver.close();
+        log.info("WebDriver closed");
+    }
 }
--- expandtesting.com/src/test/java/lt/techin/expandtesting/CreateLoginLogoutTest.java
+++ expandtesting.com/src/test/java/lt/techin/expandtesting/CreateLoginLogoutTest.java
@@ -2,43 +2,63 @@ package lt.techin.expandtesting;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+
+import static java.lang.invoke.MethodHandles.lookup;
+import static org.slf4j.LoggerFactory.getLogger;
 
 
 public class CreateLoginLogoutTest extends BasePage{
+    private static final Logger log = getLogger(lookup().lookupClass());
+
     CreateLoginLogout action;
 
     @Test
     void CreateAccount(){
         action = new CreateLoginLogout(driver);
         action.clickCreateAccount();
+        log.info("Clicked on Create Account");
         String URL = driver.getCurrentUrl();
         Assertions.assertEquals(URL, "https://practice.expandtesting.com/notes/app/register");
+        log.info("Asserted that the current url is the correct one");
         action.inputEmail("pobytest123@test.com");
         action.inputName("Poby");
         action.inputPassword("poby123");
         action.inputConfirmPassword("poby123");
         action.clickRegister();
+        log.info("Filled all fields with correct info and clicked register");
         Assertions.assertEquals("User account created successfully", action.assertSuccess());
+        log.info("Asserted that the account was created successfully");
         action.clickLogin();
+        log.info("Clicked login");
         String loginURL = driver.getCurrentUrl();
         Assertions.assertEquals(loginURL, "https://practice.expandtesting.com/notes/app/login");
+        log.info("Asserted that the current url is the correct one");
         action.inputLoginEmail("pobytest123@test.com");
         action.inputLoginPassword("poby123");
         action.clickLoginButton();
+        log.info("Filled all fields with correct info");
         Assertions.assertTrue(action.logoutIsDisplayed());
+        log.info("Asserted that we're logged in");
         action.clickLogout();
+        log.info("Clicked logout");
     }
     CreateLoginLogout login;
     @Test
     void LoginWithExistingAccount(){
         login = new CreateLoginLogout(driver);
         login.clickLoginExisting();
+        log.info("Clicked on Login");
         String loginURL = driver.getCurrentUrl();
         Assertions.assertEquals(loginURL, "https://practice.expandtesting.com/notes/app/login");
+        log.info("Asserted that the current url is the correct one");
         login.inputLoginEmail("pobytest123@test.com");
         login.inputLoginPassword("poby123");
         login.clickLoginButton();
+        log.info("Filled all fields with correct info and clicked login");
         Assertions.assertTrue(login.logoutIsDisplayed());
+        log.info("Asserted that we're logged in");
         login.clickLogout();
+        log.info("Clicked logout");
     }
 }
